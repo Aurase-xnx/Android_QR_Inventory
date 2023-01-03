68b0def6d4eb885e9c34bc11c4ba88e9ed652c98
Aurase-xnx <aurasebox@gmail.com>
2023-01-03 14:44:46 +0100

    Update

--- app/src/main/java/com/example/dilhan_eser_project/MainMenuActivity.kt
+++ app/src/main/java/com/example/dilhan_eser_project/MainMenuActivity.kt
@@ -1,5 +1,6 @@
 package com.example.dilhan_eser_project
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
@@ -8,12 +9,16 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainMenuActivity : AppCompatActivity() {
     private lateinit var mUsername: TextView
+    private lateinit var mAdmin: Button
+    private lateinit var mInventory: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_mainmenu)
+        setContentView(R.layout.activity_main_menu)
 
         mUsername = findViewById(R.id.mainmenu_username)
+        mAdmin = findViewById(R.id.mainmenu_Admin)
+        mInventory = findViewById(R.id.mainmenu_Inventory)
 
         val username = intent.getStringExtra("username")
         val formattedString = String.format("Bonjour %s", username)
@@ -21,6 +26,14 @@ class MainMenuActivity : AppCompatActivity() {
         if (username == "Admin"){
             val button = findViewById<Button>(R.id.mainmenu_Admin)
             button.visibility = View.VISIBLE
+        }
+        mAdmin.setOnClickListener(){
+            val intent = Intent(this, UserManagementActivity::class.java)
+            startActivity(intent)
+        }
+        mInventory.setOnClickListener(){
+            val intent = Intent(this, InventoryActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/example/dilhan_eser_project/UserManagementActivity.kt
@@ -0,0 +1,12 @@
+package com.example.dilhan_eser_project
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserManagementActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_user_management)
+    }
+}
